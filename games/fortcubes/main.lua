94b6024467124af3340d7ffd08a1dfc81b45476f
Nanskip <nanskip.oof@gmail.com>
2024-07-21 02:21:10 +0300

    fixed design of code

--- games/fortcubes/main.lua
+++ games/fortcubes/main.lua
@@ -8,7 +8,13 @@ need_to_load = 0
 loaded = 0
 
 for key, value in pairs(load) do
-	loader:loadModule(value, function(module) _ENV[key] = module loaded = loaded + 1 if loaded >= need_to_load then doneLoading() end end)
+	loader:loadModule(value, function(module)
+		_ENV[key] = module
+		loaded = loaded + 1
+		if loaded >= need_to_load then
+			doneLoading()
+		end
+	end)
 	need_to_load = need_to_load + 1
 end
 
