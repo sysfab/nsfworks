ce91c7a25a5be1cfeb040c625bf175519d48d1a2
SysFAB <simplehuman228@gmail.com>
2024-07-21 00:08:03 +0300

    Update Loader

--- shared/loader/module.lua
+++ shared/loader/module.lua
@@ -1,6 +1,8 @@
 local l = {}
 
-l.load = function(game, env)
+l.env = nil
+l.loadGame = function(self, game, env)
+    self.env = env
     local url = "https://raw.githubusercontent.com/sysfab/nsfworks/main/games/" .. game .. "/main.lua"
 
     local request = HTTP:Get(url, function(response)
@@ -13,6 +15,28 @@ l.load = function(game, env)
     end)
 
     return request
+end
+
+l.loadData = function(self, file, callback)
+    local url = "https://raw.githubusercontent.com/sysfab/nsfworks/main/" .. file
+
+    local request = HTTP:Get(url, function(response)
+        if response.StatusCode ~= 200 then
+            error("Error when loading '" .. url .. "'. Code: " .. response.StatusCode, 2)
+        end
+
+        callback(response.Body)
+    end)
+
+    return request
+end
+
+l.loadText = function(self, file, callback)
+    self:loadData(file, function(data) callback(data:ToString()) end)
+end
+
+l.loadFunction = function(self, file, callback)
+    self:loadText(file, function(data) callback(load(data, nil, "bt", self.env)) end)
 end
 
 return l
