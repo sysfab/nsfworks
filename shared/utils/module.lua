43bc853cb23c83a0a23b28d3b4d1e4d0bbc08441
SysFAB <simplehuman228@gmail.com>
2024-07-21 01:05:36 +0300

    mapRange function [utils.lua]

--- shared/utils/module.lua
+++ shared/utils/module.lua
@@ -4,6 +4,12 @@ utils.init = function(env)
 	env.lerp = function(a, b, w)
 		return a + (b-a)*w
 	end
+
+	env.mapRange = function(fromRange, value, toRange)
+	    local fromMin, fromMax = fromRange[1], fromRange[2]
+	    local toMin, toMax = toRange[1], toRange[2]
+	    return (value - fromMin) * (toMax - toMin) / (fromMax - fromMin) + toMin
+	end
 end
 
 return utils
