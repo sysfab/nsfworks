785d0a82d94ebd3e7ffc6f013da4293a8a336037
SysFAB <simplehuman228@gmail.com>
2024-07-21 03:28:37 +0300

    update main.lua

--- games/fortcubes/main.lua
+++ games/fortcubes/main.lua
@@ -4,16 +4,19 @@ load = {
 	loading_screen = "games/fortcubes/loading_screen.lua",
 	menu = "games/fortcubes/menu.lua"
 }
-need_to_load = 0
-loaded = 0
 
 for key, value in pairs(load) do
+	if need_to_load == nil then need_to_load = 0 end
 	need_to_load = need_to_load + 1
 
 	loader:loadModule(value, function(module)
 		debug.log("main() - Loaded '".. key .."'")
+
 		_ENV[key] = module
+
+		if loaded == nil then loaded = 0 end
 		loaded = loaded + 1
+
 		if loaded >= need_to_load then
 			doneLoading()
 		end
