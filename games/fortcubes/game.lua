010c1628e91eabe0bc5b4d609673a1cc1a7d1ea6
SysFAB <simplehuman228@gmail.com>
2024-07-22 23:43:45 +0300

    game.lua upd 1

--- games/fortcubes/game.lua
+++ games/fortcubes/game.lua
@@ -9,4 +9,13 @@ game.controls.remove = function(controls)
 
 end
 
+
+game.create = function(self)
+	self.controls:create()
+end
+game.remove = function(self)
+	self.controls:remove()
+end
+
+
 return game
