95df73389147a4a78a50f9796f6f7fab723d3b84
SysFAB <simplehuman228@gmail.com>
2024-07-22 23:47:53 +0300

    added moveJoystic

--- games/fortcubes/game.lua
+++ games/fortcubes/game.lua
@@ -3,10 +3,15 @@ local game = {}
 game.controls = {}
 game.controls.created = false
 game.controls.create = function(controls)
-
+	game.controls.moveJoystick = joysticks.create({
+		pos = {0, 0}, -- position on screen.
+		scale = 1, -- scale multiplier, 1 = 160 pixels.
+		color = Color(255, 255, 255, 127), -- color of joystick's insides.
+		borderColor = Color(255, 255, 255, 255) -- color of joystick's border.
+	})
 end
 game.controls.remove = function(controls)
-
+	game.controls.moveJoystick:remove()
 end
 
 
