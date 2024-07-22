76849c66c13052597f89f531ec8d34c4f0e062b3
SysFAB <simplehuman228@gmail.com>
2024-07-22 23:55:01 +0300

    game.lua upd 2

--- games/fortcubes/game.lua
+++ games/fortcubes/game.lua
@@ -2,10 +2,20 @@ local game = {}
 
 game.controls = {}
 game.controls.created = false
+game.controls.screenResize = function(controls)
+	controls.moveJoystick:setPos(Number2(10, 10))
+	controls.moveJoystick:setPos(Number2(Screen.Width-10, 10))
+end
 game.controls.create = function(controls)
-	game.controls.moveJoystick = joysticks.create({
-		pos = {0, 0}, -- position on screen.
-		scale = 1, -- scale multiplier, 1 = 160 pixels.
+	controls.moveJoystick = joysticks.create({
+		pos = {10, 10}, -- position on screen.
+		scale = 1.1, -- scale multiplier, 1 = 160 pixels.
+		color = Color(255, 255, 255, 127), -- color of joystick's insides.
+		borderColor = Color(255, 255, 255, 255) -- color of joystick's border.
+	})
+	controls.shootJoystick = joysticks.create({
+		pos = {Screen.Width-10, 10}, -- position on screen.
+		scale = 1.1, -- scale multiplier, 1 = 160 pixels.
 		color = Color(255, 255, 255, 127), -- color of joystick's insides.
 		borderColor = Color(255, 255, 255, 255) -- color of joystick's border.
 	})
@@ -14,12 +24,23 @@ game.controls.remove = function(controls)
 	game.controls.moveJoystick:remove()
 end
 
-
+game.created = false
+game.screenResize = function(self)
+	if self.created ~= true then return end
+	self.controls:screenResize()
+end
 game.create = function(self)
+	self.created = true
 	self.controls:create()
+	self:screenResize()
+
+	self.screenResizeListener = LocalEvent:Listen(LocalEvent.Name.ScreenDidResize, function()
+        self:screenResize()
+    end)
 end
 game.remove = function(self)
 	self.controls:remove()
+	self.created = false
 end
 
 
