cdc0223cf6eea09df45411d5b0085bb68e586ad6
SysFAB <simplehuman228@gmail.com>
2024-07-22 18:34:33 +0300

    add version text

--- games/fortcubes/menu.lua
+++ games/fortcubes/menu.lua
@@ -88,6 +88,8 @@ function menu.create(self)
     self.titleBG = ui:createFrame(Color(0, 0, 0, 50))
     self.title2 = ui:createText("FORTCUBES", Color(0, 0, 0, 127))
     self.title = ui:createText("FORTCUBES", Color(255, 255, 255, 255))
+    self.versionBG = ui:createFrame(Color(0, 0, 0, 50))
+    self.version = ui:createText(VERSION, Color(0, 0, 0, 127))
     menu.man1 = Quad() menu.man1:SetParent(World)
     menu.man2 = Quad() menu.man2:SetParent(World)
     menu.man3 = Quad() menu.man3:SetParent(World)
@@ -217,6 +219,12 @@ function menu.create(self)
         menu.title2.object.Scale.X = menu.screenWidth * 8.85
         menu.title2.object.Scale.Y = menu.screenHeight * 8.85
         menu.title2.pos = Number2(11+30 * menu.screenWidth, Screen.Height - Screen.SafeArea.Top - menu.titleBG.Height - 32+72/2+5)
+
+        menu.versionBG.Width = menu.version.Width + 6
+        menu.versionBG.Height = menu.version.Height + 6
+        menu.versionBG.pos = Number2(menu.titleBG.pos.X + menu.titleBG.Width - menu.versionBG.Width, menu.titleBG.pos.Y-menu.versionBG.Height)
+
+        menu.version.pos = Number2(menu.versionBG.pos.X + 3, menu.versionBG.pos.Y + 3)
 
         -- MAIN MENU -- BUTTONS
         for k, v in pairs(self.menus) do
