eec3b619fd775c731bcd90360b58ee7ab6eef3f2
SysFAB <simplehuman228@gmail.com>
2024-07-23 15:52:52 +0300

    Server.lua upd 1

--- games/fortcubes/server.lua
+++ games/fortcubes/server.lua
@@ -1,2 +1,18 @@
 debug.enabled = true
 debug.log("server() - online")
+
+Server.OnPlayerJoin = function(player)
+	debug.log("server() - player joined [" .. player.Username .. "]")
+end
+
+Server.OnPlayerLeave = function(player)
+	debug.log("server() - player leaved [" .. player.Username .. "]")
+end
+
+Server.DidReceiveEvent = function(e)
+	crystal.ParseEvent(e, {
+		"_" = function(event)
+			debug.log("server() - got unknown event: "..tostring(event.action))
+		end
+	})
+end
