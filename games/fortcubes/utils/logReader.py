8790a5174f68350704d933ec462842e37b3216f3
SysFAB <simplehuman228@gmail.com>
2024-07-27 03:33:19 +0300

    update logs reader

--- games/fortcubes/utils/logReader.py
+++ games/fortcubes/utils/logReader.py
@@ -2,5 +2,16 @@ import json
 
 logs = []
 with open("logs.json", "r", encoding="UTF-8") as f:
-	for log in json.loads(f.read()):
-		print(("[DEBUG] " if log['type'] == "message" else "[DEBUG ERROR] ") + str(log['content']))
+	content = json.loads(f.read())
+	if type(content) == list:
+		for log in content:
+			print(("[DEBUG] " if log['type'] == "message" else "[DEBUG ERROR] ") + str(log['content']))
+	elif type(content) == dict:
+		print("CLIENT LOGS:")
+		for log in content['client']:
+			print(("[DEBUG] " if log['type'] == "message" else "[DEBUG ERROR] ") + str(log['content']))
+		print("\nSERVER LOGS:")
+		for log in content['client']:
+			print(("[DEBUG] " if log['type'] == "message" else "[DEBUG ERROR] ") + str(log['content']))
+	else:
+		raise ValueError("Wrong logs format")
