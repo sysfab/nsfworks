ea4d0736e9bea5d66f776ad60964f0a3d860e54e
Nanskip <nanskip.oof@gmail.com>
2024-07-29 02:23:41 +0300

    fixed rotation copy

--- games/fortcubes/game.lua
+++ games/fortcubes/game.lua
@@ -585,7 +585,7 @@ game.connection.onEvent = function(connection, e)
 				bush.type = "bush"
 
 				bush.move = function(self)
-					local defaultRot = self.Rotation
+					local defaultRot = Rotation(self.Rotation.X, self.Rotation.Y, self.Rotation.Z)
 					self.Rotation.X = math.random(-10, 10)*0.1
 					for i=1, 60 do
 						Timer(i*0.016, false, function()
