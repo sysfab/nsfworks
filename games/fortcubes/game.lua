58197f99fe5f46967d8ea69df9f79caebc42a71f
Nanskip <nanskip.oof@gmail.com>
2024-08-01 00:52:47 +0300

    fixed timerBG width and height

--- games/fortcubes/game.lua
+++ games/fortcubes/game.lua
@@ -771,8 +771,8 @@ game.ui.create = function(u)
 		if u.timer ~= nil and u.loadedTimer then
 			u.timer.pos = Number2(Screen.Width/2-Screen.SafeArea.Right-u.timer.Width/2, Screen.Height-Screen.SafeArea.Top-u.timer.Height-15)
 			u.timerBG.pos = Number2(u.timerBG.pos.X-15, u.timerBG.pos.Y-15)
-			u.timerBG.Width = u.timerBG.Width + 30
-			u.timerBG.Height = u.timerBG.Height + 30
+			u.timerBG.Width = u.timer.Width + 30
+			u.timerBG.Height = u.timer.Height + 30
 
 			local minutes = (game.time_end - math.floor(game.time))//60
 			local seconds = (game.time_end - math.floor(game.time))%60
