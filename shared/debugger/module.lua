ad50ba825e30fe28e3c06785f765921b486ec675
SysFAB <simplehuman228@gmail.com>
2024-08-04 10:59:56 +0300

    debug.getLine

--- shared/debugger/module.lua
+++ shared/debugger/module.lua
@@ -46,6 +46,13 @@ debug.assert = function(condition, message, level)
 	end
 end
 
+debug.getLine = function()
+	local ok, err = pcall(function() error("", 2) end)
+	if not ok then
+		return err
+	end
+end
+
 
 debug.export = function(self)
 	if self.error == nil then
