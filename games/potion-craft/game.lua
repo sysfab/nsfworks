9e2be0b5c1e59bd94f00821d9ad2cfc29182976d
SysFAB <simplehuman228@gmail.com>
2024-08-07 19:22:02 +0300

    wow

--- games/potion-craft/game.lua
+++ games/potion-craft/game.lua
@@ -7,12 +7,12 @@ game.create = function(self)
     Player.Velocity = Number3(0, 0, 0)
 
     self.map = Shape(shapes.map, {includeChildren = true})
-    self.map.Scale = 5
+    self.map.Scale = 25
     self.map.Physics = PhysicsMode.StaticPerBlock
     self.map:SetParent(World)
     self.map.Position = Number3(0, 0, 0)
 
-    Player.Position = Number3(self.map.Width/2, self.map.Height, self.map.Depth/2)
+    Player.Position = Number3(self.map.Width/2, self.map.Height, self.map.Depth*0.35)
 end
 
 game.remove = function(self)
