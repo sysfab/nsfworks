f95826b54f64777fdd52072a57d5f402f8eb79bf
SysFAB <simplehuman228@gmail.com>
2024-08-24 07:52:53 +0300

    add utils.copyTable

--- shared/utils/module.lua
+++ shared/utils/module.lua
@@ -42,6 +42,28 @@ utils.init = function(self, env)
 	self.env.loadFunction = function(func_or_string, env)
 		return load(func_or_string, nil, "bt", self.env)
 	end
+
+	--http://lua-users.org/wiki/CopyTable
+	self.env.copyTable = function(orig, copies)
+		copies = copies or {}
+		local orig_type = type(orig)
+		local copy
+		if orig_type == 'table' then
+			if copies[orig] then
+				copy = copies[orig]
+			else
+				copy = {}
+				copies[orig] = copy
+				for orig_key, orig_value in next, orig, nil do
+					copy[deepcopy(orig_key, copies)] = deepcopy(orig_value, copies)
+				end
+				setmetatable(copy, deepcopy(getmetatable(orig), copies))
+			end
+		else -- number, string, boolean, etc
+			copy = orig
+		end
+		return copy
+	end
 end
 
 return utils
