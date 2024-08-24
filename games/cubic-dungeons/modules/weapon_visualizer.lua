ae9f06dd67594a2c9b3ae858025965b7ab2b1eaa
SysFAB <simplehuman228@gmail.com>
2024-08-24 12:28:57 +0300

    update weapon visualizer

--- games/cubic-dungeons/modules/weapon_visualizer.lua
+++ games/cubic-dungeons/modules/weapon_visualizer.lua
@@ -3,6 +3,8 @@ local visualizer = {}
 visualizer.create = function(weapon)
     local v = {}
 
+    v.weapon = weapon
+
     v.window = ui:createFrame(Color(0, 0, 0, 125))
     v.window.parentDidResize = function(s)
         s.Width = Screen.Width
@@ -10,19 +12,49 @@ visualizer.create = function(weapon)
     end
     v.window:parentDidResize()
 
-    v.title = ui:createText(weapon.Name, Color(255, 255, 255))
+    v.title = ui:createText(v.weapon.Name, Color(255, 255, 255))
     v.title:setParent(window)
     v.title.parentDidResize = function(s)
         s.pos = Number2(v.window.Width/2 - s.Width/2, v.window.Height - s.Height - 30)
     end
     v.title:parentDidResize()
 
+    v.description = ui:createText(v.weapon.Description, Color(220, 220, 220))
+    v.description:setParent(window)
+    v.description.parentDidResize = function(s)
+        s.pos = Number2(v.window.Width/2 - s.Width/2, v.window.Height - v.title.Height - 16 - s.Height - 30)
+    end
+    v.description:parentDidResize()
+
+    for i, part in ipairs(v.weapon.Parts) do
+        v["part "..part.Name] = ui:createFrame()
+        local p = v["part "..part.Name]
+
+        p:setImage(part.Texture)
+        p:setParent(v.window)
+        p.parentDidResize = function(s)
+            local wh = math.min(Screen.Width/3, Screen.Height/3)
+
+            p.Width = wh
+            p.Height = wh
+        end
+        p:parentDidResize()
+    end
+
     v.remove = function(self)
         self.window:remove()
         self.title:remove()
+        self.description:remove()
+
+        for i, part in ipairs(v.weapon.Parts) do
+            local p = v["part "..part.Name]
+            p:remove()
+            v["part "..part.Name] = nil
+        end
 
         self.title = nil
         self.window = nil
+        self.description = nil
     end
 
     return v
