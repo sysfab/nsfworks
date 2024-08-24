71c1335855c881c5753a07a17fed556dd8d6b940
SysFAB <simplehuman228@gmail.com>
2024-08-24 08:51:41 +0300

    added debug

--- games/cubic-dungeons/client.lua
+++ games/cubic-dungeons/client.lua
@@ -231,6 +231,7 @@ function doneLoading()
 			local wp_config = copyTable(part)
 			for i, effect in ipairs(part.stat_effects) do
 				local code = effect:gsub("(%w+)%s*([%+%-/%*])=%s*(%d+)", "%1 = %1 %2 %3")
+				Debug.log("Loaded effect: "..code)
 				wp_config.stat_effects[i] = function(stats)
 					loadFunction(code, stats)()
 				end
